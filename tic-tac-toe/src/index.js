2b2274e21f8e64ee2c8e3ec1fc88ca541de5e021
lucasr <lrodriguez.arevalosc@gmail.com>
2022-05-26 13:39:29 -0300

    agregando comentarios dentre de metodos

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -17,7 +17,7 @@ ShoppingList.defaultProps = {list : "empty"}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App arr={["1","2"]}/>
+    {/* <App arr={["1","2"]}/> */}
     <ShoppingList />
   </React.StrictMode>
 );
