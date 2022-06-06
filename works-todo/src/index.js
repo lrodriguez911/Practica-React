e36615f7e36bbcb2d89cf65625a819b48cc5525e
lucasr <lrodriguez.arevalosc@gmail.com>
2022-06-06 14:16:16 -0300

    add counter and controller inputs

--- works-todo/src/index.js
+++ works-todo/src/index.js
@@ -81,4 +81,74 @@ class TodoList extends React.Component {
     );
   }
 }
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // Cambia el código debajo de esta línea
+this.handleChange = this.handleChange.bind(this)
+    // Cambia el código encima de esta línea
+  }
+  // Cambia el código debajo de esta línea
+handleChange(event){
+this.setState({input : event.target.value})
+}
+  // Cambia el código encima de esta línea
+  render() {
+    return (
+      <div>
+        { /* Cambia el código debajo de esta línea */}
+<input type='text' value={this.state.input}
+onChange={this.handleChange}/>
+        { /* Cambia el código encima de esta línea */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    // Cambia el código debajo de esta línea
+this.increment = this.increment.bind(this)
+this.decrement = this.decrement.bind(this)
+this.reset = this.reset.bind(this)
+    // Cambia el código encima de esta línea
+  }
+  // Cambia el código debajo de esta línea
+increment(){
+  return this.setState((state)=>({
+    count : state.count +1
+  }))
+}
+decrement(){
+  return this.setState((state) => ({
+    count : state.count - 1
+  }))
+}
+reset(){
+  return this.setState(() =>(
+    {count : 0}
+  ))
+}
+  // Cambia el código encima de esta línea
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+};
 root.render(<WorksTodo />); 
+root.render(<ControlledInput />); 
+root.render(<Counter />); 
