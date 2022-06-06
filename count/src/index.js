6b2420df86118f82838b90ce6fe88f13658699c5
lucasr <lrodriguez.arevalosc@gmail.com>
2022-06-06 14:11:25 -0300

    agregado alÃgunos

--- count/src/index.js
+++ count/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import useState from 'react';
 
+function Contador (){
+  const [sta, setState] = useState();
+  console.log(sta)
+  return(<div>
+    <button onClick={()=>
+      setState(({sta : 0 }))
+    }>Incrementar</button>
+  </div>)
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.Fragment>
+    <App/>
+    <Contador />
+  </React.Fragment>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
