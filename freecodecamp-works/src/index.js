eedf5f8ec35d70626d11acb25959900d2fa29a3d
lucasr <lrodriguez.arevalosc@gmail.com>
2022-06-08 14:19:20 -0300

    adds works of freecodecamp

--- freecodecamp-works/src/index.js
+++ freecodecamp-works/src/index.js
@@ -74,17 +74,198 @@ class MyForm extends React.Component {
     );
   }
 }
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         {/* pasando el state del padre a la props name del hijo*/}
+         <Navbar name={this.state.name}/>
+         
+       </div>
+    );
+  }
+};
 
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      {/*pasando una props a un elemento hijo, depues puedo pasar el state del padre como props */}
+      <h1>Hello, my name is:{this.props.name} </h1>
+    </div>
+    );
+  }
+};
+class MyAppCb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+       <div>
+        { /* ingreso los componentes hijos a renderizar y agrego un callback como props */ }
+<GetInput 
+input={this.state.inputValue} 
+handleChange={this.handleChange}
+/>
+<RenderInput input={this.state.inputValue}/>
+        
+       </div>
+    );
+  }
+};
 
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
 
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
+class MyComponentLifeCycle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // learning about the lifecycle in react
+console.log('hola')
+  }
+  render() {
+    console.log('hola1')
+    return(<div />)
+   
+  }
+};
+class MyComponentEvents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ''
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // the componentDidMount method is a best place to attach any envents listeners, in this add addEventListener
+  // and in conponentWillUnmount remove envent listener
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown',this.handleKeyPress)
+  }
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + 'You pressed the enter key! '
+    }));
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+};
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps) {
+    console.log('Should I update?');
+    // shouldComponentUpdate renderer the component only if a return true, in this case re-renderer
+    // the component if props only evens
+    return nextProps.value % 2 === 0;
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.Fragment>
   <React.StrictMode>
     <App />
     <MyComponent />
     <MyForm />
+    <MyApp />
+    <MyAppCb />
+    <MyComponentLifeCycle />
+    <MyComponentEvents />
+    <Controller />
   </React.StrictMode>
+  </React.Fragment>
 );
 
 
