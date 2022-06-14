50328a9dfade142dc5d6b3cd68af50eb94acef3d
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-06-13 21:16:11 -0300

    add four exercises about ternary operator and conditional operator

--- freecodecamp-works/src/index.js
+++ freecodecamp-works/src/index.js
@@ -380,6 +380,144 @@ else {
     )};
   }
 };
+class MyComponentConditionalRender extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+       </div>
+    );
+  }
+};
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {/* Cambia el código debajo de esta línea */}
+    return (<h1>{this.props.fiftyFifty ? 'You Win!' : 'You Lose!'}</h1>)
+    {/* Cambia el código encima de esta línea */}
+  }
+}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(
+      {
+        counter: this.state.counter + 1
+      }
+    );
+  }
+  render() {
+    const expression = Math.random() <= 0.5 ? true : false;
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+<Results fiftyFifty={expression} />
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+  }
+}
+const inputStyle1 = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+this.state = {
+  input: '',
+  userAge: '',
+}
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle1}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+{!this.state.userAge ? buttonOne : this.state.userAge < 18 ? buttonThree :
+buttonTwo}
+        {/* using operator ternaty to conditional style */}
+      </div>
+    );
+  }
+}
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    // cahnge the style depending of the state
+    this.state.input.length > 15 ? 
+    inputStyle ={
+      border: '3px solid red'
+    } : '';
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Fragment>
@@ -396,6 +534,10 @@ root.render(
     <Colorful1 />
     <MagicEightBall />
     <MyComponentConditional />
+    <MyComponentConditionalRender />
+    <GameOfChance />
+    <CheckUserAge />
+    <GateKeeper />
   </React.StrictMode>
   </React.Fragment>
 );
