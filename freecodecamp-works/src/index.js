eaca77d8fd188340169c6ddd5dea03d71ba9f7bf
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-06-15 22:59:41 -0300

    start reducer, actions and stores in redux

--- freecodecamp-works/src/index.js
+++ freecodecamp-works/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+
 //component use to change state of visibility
 class MyComponent extends React.Component {
   constructor(props) {
@@ -663,7 +664,28 @@ root.render(
 );
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const counterReducer = (state = 0, action) => {
+  const {type} = action;
+  switch (action.type) {
+    case '@counter/incremented':
+      return state +1;
+    case '@counter/decremented':
+      return state +1;
+    case '@counter/reset':
+      return state = 0;
+    default:
+      return state;
+  }
+}
+const actionIncremented = {
+  type : '@counter/incremented'
+}
+const actionDecremented = {
+  type : '@counter/decremented'
+}
+const actionReset = {
+  type : '@counter/reset'
+}
+counterReducer(1, actionIncremented)
+counterReducer(1, actionDecremented)
+counterReducer(10, actionReset)
