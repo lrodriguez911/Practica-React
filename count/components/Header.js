915572d2f61b04189a755078894f2f471e376e41
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-06-27 20:54:35 -0300

    adding header whit img and a

--- count/components/Header.js
+++ count/components/Header.js
@@ -1,7 +1,8 @@
 function Hearder() {
     return(
         <div>
-            <button>Inicio</button>
+            <img src="\src\logo.svg" alt="logo"></img>
+            <button><a href="www.google.com">Inicio</a></button>
             <button>Principal</button>
             <button>Contacto</button>
             <button>Login</button>
