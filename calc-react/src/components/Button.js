aa940699840c10894e2bfc405687ba35f25ae638
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-01-16 20:56:40 -0300

    continue whit calc application

--- calc-react/src/components/Button.js
+++ calc-react/src/components/Button.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import '../stylesheets/Button.css'
 
 function Button(props) {
+
+    const isOperator = (value) => {
+        return isNaN(value) && (value !== '.') && (value !== '=')
+    };
     return(
-        <div>
+        <div className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
             {props.children}
         </div>
     )
