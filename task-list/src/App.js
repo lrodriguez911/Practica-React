4b08f3c8b35521c901dbbfeace9546dec437bf32
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-01-19 23:16:30 -0300

    start makeing the first component

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./images/logopm.png"
+import Task from "./components/Task";
 
 function App() {
   return (
     <div className="app-task-list">
-      \
+      <div className="logo-container">
+        <img src={logo} alt="logo ofplay more" className="logo-playmore" />
+      </div>
+      <div className="tasks-principal-list">
+        <h1>My Tasks</h1>
+        <Task text='hola'/>
+      </div>
     </div>
   );
 }
---
+++ task-list/src/components/Task.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../stylesheets/Task.css'
+function Task({ text }) {
+    return (
+        <div className="task-container">
+            <div className="task-text">
+                {text}
+            </div>
+            <div className="task-icon">
+                Delete
+            </div>
+        </div>
+    )
+}
+
+export default Task;
